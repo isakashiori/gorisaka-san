56937bec52d3bf6d244c3df1f94dfdc555f715ca
isakashiori <isslamdunk0000@gmail.com>
2023-02-14 17:50:50 +0900

    add: おうむ返しの設定

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -1,7 +1,26 @@
 class LinebotsController < ApplicationController
   protect_from_forgery except: [:callback]
   def callback
-
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      return head :bad_request
+    end
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+    head :ok
   end
 
   private
