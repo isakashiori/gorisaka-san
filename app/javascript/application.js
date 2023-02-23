9dc36d379779f6c276f2fa3c20a57448d20cf6f5
isakashiori <isslamdunk0000@gmail.com>
2023-02-23 16:48:30 +0900

    add: daisyuiのインストールとフラッシュメッセージの追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,1 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// Entry point for the build script in your package.json
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -25,5 +25,6 @@ module.exports = {
     }),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require("daisyui")
   ]
 }
