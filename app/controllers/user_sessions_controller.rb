cc7be690b305ce2802f66064b08a691d6064c37f
isakashiori <isslamdunk0000@gmail.com>
2023-02-26 18:20:54 +0900

    add: エラー時のフラッシュメッセージを追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -17,8 +17,9 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path
+      redirect_back_or_to root_path, success: 'ログインしました'
     else
+      flash.now[:error] = 'ログインに失敗しました'
       render :new
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      redirect_to login_path, success: '新規登録が完了しました'
     else
+      flash.now[:error] = '新規登録に失敗しました'
       render :new
     end
   end
